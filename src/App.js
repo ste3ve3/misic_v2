b5aded5458f5f36b4f895a8d529d505ace851e09
Steve Ndicunguye <ndicunguyesteve4@gmail.com>
2023-05-27 10:25:48 +0200

    deploying to heroku

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import themes from "themes";
 import NavigationScroll from "layout/NavigationScroll";
 import { Toaster } from "react-hot-toast";
 
-// ==============================|| APP ||============================== //
+// ==============================|| MAIN APP ||============================== //
 
 const App = () => {
   const customization = useSelector((state) => state.customization);
